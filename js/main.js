7c3c210d90352347d4e1ed2006aa3c907a065330
JuliaMukh <mukhina_ur@mail.ru>
2025-03-09 22:19:37 +0500

    add range with one thumb

--- js/main.js
+++ js/main.js
@@ -22,3 +22,14 @@ document.addEventListener('DOMContentLoaded', function() {
     });
 });
 
+
+let rangePrice = document.querySelector('input[type="range"].filter-price__slider');
+rangePrice.style.setProperty('--value', rangePrice.value);
+rangePrice.style.setProperty('--min', rangePrice.min == '' ? '0' : rangePrice.min);
+rangePrice.style.setProperty('--max', rangePrice.max == '' ? '100' : rangePrice.max);
+rangePrice.addEventListener('input', 
+    () => rangePrice.style.setProperty('--value', rangePrice.value)
+);
+
+
+
