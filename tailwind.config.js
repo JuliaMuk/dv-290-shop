c6fd8c30f35369d6220573539229c5e8007cdb61
JuliaMukh <mukhina_ur@mail.ru>
2025-03-17 21:10:37 +0500

    install tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./**/*.{html,js}'],
+  darkMode: 'html',
+  theme: {
+    
+    extend: { 
+      colors: {
+        customBlue: '#1DA1F2',
+        customGreen: '#10B981',
+      },
+    },
+  },
+  plugins: [
+  ],
+}
+
